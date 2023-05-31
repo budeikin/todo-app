b10c8a4ffd7564a5ae3e9f595481caf741f09817
budeikin <budeikin52@gmail.com>
2023-05-31 18:22:06 +0330

    added register and login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.views.generic import FormView
 
-# Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    fields = ['username', 'password']
+    success_url = '/'
+
+
+class RegisterView(FormView):
+    form_class = UserCreationForm
+    template_name = 'accounts/register.html'
+    success_url = '/'
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterView, self).form_valid(form)
---
+++ todo/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(max_length=255)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user
+
+    class Meta:
+        order_with_respect_to = "user"
+
+
+class TaskLine(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True,null=True)
+    task = models.ForeignKey('Task',on_delete=models.CASCADE,related_name='tasklines')
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.task.title
